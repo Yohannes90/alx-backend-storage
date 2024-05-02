afeca4b48689356e9159ba7404c999bd7761cc56
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-05-02 06:32:03 -0700

    first commit for question 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,23 @@
-jhihh
+#!/usr/bin/env python3
+""" Module for caching data using Redis
+"""
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """A class for caching data using Redis
+    """
+    def __init__(self):
+        """Cache constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
