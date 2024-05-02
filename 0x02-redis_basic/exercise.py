c1e4ebee8f46075a94e233e250497879a7bf7021
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-05-02 06:47:01 -0700

    first commit for question 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: callable = None) -> Union[str, bytes, int, float, None]:
+        """ Get data from Redis
+        """
+        if fn:
+            data = self._redis.get(key)
+            if data is None:
+                return None
+            return fn(data)
+        else:
+            return self._redis.get(key)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Retrieve a string value from based on the given key
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Retrieve an int value from Redis based on the given key
+        """
+        return self.get(key, fn=int)
